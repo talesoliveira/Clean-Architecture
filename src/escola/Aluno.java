2b44a46ec9713a01c6ff5535215bb826757ef1bb
tales <conta.tales.oliveira2016@gmail.com>
2021-08-24 22:37:17 -0300

    estudos clean-architecture

--- src/escola/Aluno.java
+++ src/escola/Aluno.java
@@ -1,4 +1,41 @@
 package escola;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aluno {
+
+    private Cpf cpf;
+    private String nome;
+    private Email email;
+
+    private List<Telefone> telefones = new ArrayList<>();
+
+    public Aluno(Cpf cpf, String nome, Email email){
+        this.cpf = cpf;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public void adicionarTelefone(String ddd, String numero){
+        this.telefones.add(new Telefone());
+    }
+
+    public Cpf getCpf(){
+        return cpf;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public Email getEmail(){
+        return email;
+    }
+
+
+
+
+
 }
--- src/escola/Cpf.java
+++ src/escola/Cpf.java
@@ -1,4 +1,17 @@
 package escola;
 
 public class Cpf {
+
+    private String numero;
+
+    public Cpf(String numero) {
+        if(numero == null || !numero.matches("\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}")){
+            throw new IllegalArgumentException("CPF invalido!");
+        }
+    }
+
+    public String getNumero(){
+        return numero;
+    }
+
 }
